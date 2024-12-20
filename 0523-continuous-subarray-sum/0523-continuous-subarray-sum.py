4452614f547bf332547ca8c5920f272c2e103e06
Nathan Fisseha <nathan12fisseha@gmail.com>
2024-12-20 05:26:05 -0800

    Time: 46 ms (91.99%), Space: 38.1 MB (9.91%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        if len(nums) < 2:
-            return False
-        sume = 0  
-        map = {0: -1}
-        for i in range(len(nums)):
-            sume+= nums[i]
-            if sume%k not in map:
-                map[sume%k] = i
+        map = {0:-1}
+        sume = 0 
+        for index,num in enumerate(nums):
+            sume+=num
+            rem = sume%k
+            if rem not in map:
+                map[rem] = index
             else:
-                if i-map[sume%k] > 1:
-                    print(i,map)
-                    return True           
-        return False               
+                if index - map[rem] > 1:
+                    return True
+        return False                  
         
