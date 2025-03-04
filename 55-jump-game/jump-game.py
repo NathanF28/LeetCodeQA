90d152d4ec1f1e13654779affa6cfb74101cf02f
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-03-04 17:39:23 +0300

    Time: 7 ms (97.57%) | Memory: 18.7 MB (50.04%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        possible = len(nums)-1
+        for i in range(len(nums)-1,-1,-1):
+            if i + nums[i] >= possible:
+                possible = i
+        return possible == 0
+
+        
