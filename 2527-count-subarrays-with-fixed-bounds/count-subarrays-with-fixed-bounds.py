3ff5ea9636546f56472e7ed00e29c480f554c56b
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-03-19 19:26:38 +0300

    Time: 182 ms (21.39%) | Memory: 28.9 MB (46.91%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -1,16 +1,19 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        invalid_start = -1     #left most bound, where it starts to be valid
+        inv = -1
         minPos = -1
         maxPos = -1
         count = 0
         for i in range(len(nums)):
             if nums[i] < minK or nums[i] > maxK:
-                invalid_start = i   # this number can never be included in the array, it disrupts it so we mark it as the last surely invalid place
+                inv = i
             if nums[i] == minK:
-                minPos= i
+                minPos = i
             if nums[i] == maxK:
-                maxPos= i
-            count+= max(0,min(minPos,maxPos) - invalid_start) # min(minPos,maxPos) means where the valid subarray will start and the rest includes valid numbers between that place and invalid_start
-            # max(0, ...) is if invalid_start is larger 
-        return count
+                maxPos = i
+            beginning = min(minPos,maxPos)
+            count+= max(0, beginning - inv)
+        return count 
+
+        
+        
