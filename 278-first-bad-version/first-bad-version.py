97e794fc2af4f21643f98c2b5aa518adfcaab876
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-03-27 17:40:06 +0300

    Time: 32 ms (87.46%) | Memory: 17.6 MB (92.46%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -8,12 +8,7 @@ class Solution:
         while l <= r:
             mid = (l+r) //2
             if isBadVersion(mid):
-                if mid != 1 and not isBadVersion(mid-1):
-                    return mid
-                if mid == 1 and isBadVersion(mid):
-                    return 1
-                else:
-                    r = mid - 1
-
+                r = mid - 1
             else:
                 l = mid+1
+        return l
