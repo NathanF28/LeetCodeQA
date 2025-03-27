a60d0f81e17c4b0c9755e698b9dd1277b275d463
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-03-27 16:13:48 +0300

    Time: 430 ms (25.42%) | Memory: 29.7 MB (47.96%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,20 +1,22 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        sume = sum(candies)
-        if sume < k:
+        l = 1
+        r = max(candies)
+        if sum(candies) < k:
             return 0
-        count = 0
-        val = sume // k
-        l,r = 1, val
-        while l<=r:
+        def validate(n):
+            count = 0
+            for candy in candies:
+                if candy >= n:
+                    count+= candy // n
+            return count >= k
+        while l <= r:
             mid = (l+r)//2
-            res = 0
-            for c in candies:
-                if c >= mid:
-                    res+=c//mid
-            if res >= k:
-                count = mid
-                l = mid+1
+            if validate(mid):
+                l = mid + 1
             else:
                 r = mid-1
-        return count 
+        return r
+            
+
+        
