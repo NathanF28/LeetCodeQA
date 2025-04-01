50702f53f865104c7a08278883c2c34489b9259f
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-04-01 16:21:20 +0300

    Time: 0 ms (100.00%) | Memory: 23.1 MB (37.17%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -2,11 +2,9 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         l = 0
         r = max(citations)
-
         while l <=r:
             mid = (l+r) //2
             bl = bisect_left(citations,mid)
-            print(mid,bl)
             if len(citations) - bl >= mid:
                 l = mid + 1
             else:
