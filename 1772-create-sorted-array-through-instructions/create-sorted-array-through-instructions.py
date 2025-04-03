5fddbebcdf6b5c87e8afe2a90fe246dc410b3379
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-04-03 18:18:53 +0300

    Time: 3078 ms (58.33%) | Memory: 32.2 MB (86.46%) - LeetSync

--- 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.py
+++ 1772-create-sorted-array-through-instructions/create-sorted-array-through-instructions.py
@@ -1,17 +1,15 @@
-from typing import List
 from sortedcontainers import SortedList
-
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         arr = SortedList()
+        arr.add(instructions[0])
         count = 0
-        MOD = 10**9 + 7
-        
-        for num in instructions:
+        for num in instructions[1:]:
             bl = arr.bisect_left(num)
-            br = len(arr) - arr.bisect_right(num)
-            count += min(bl, br)
-            count %= MOD
+            br = arr.bisect_right(num)
+            count+= min(len(arr)-br,bl)
+            count %=  (10**9 + 7)
             arr.add(num)
+        return count  
+
         
-        return count
