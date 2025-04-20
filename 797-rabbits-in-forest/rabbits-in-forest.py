eab358c76a9768f471fa28d491a89824aff0156a
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-04-20 03:46:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (76.07%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,15 +1,17 @@
 class Solution:
-    def numRabbits(self, answers: List[int]) -> int:
-        map =  {} 
-        sume = 0
-        for answer in answers:
-            if answer in map:
-                map[answer] -=1
-                if map[answer] == -1:
-                    del map[answer]
-            if answer not in map:
-                map[answer] = answer
-                sume+= answer+1
-        return sume
-                
+    def numRabbits(self, numwers: List[int]) -> int:
+        map = {}
+        count = 0
+        for num in numwers:
+            if num+1 not in map:
+                count+= num+1
+                map[num+1] = num
+            else:
+                if map[num+1] == 0:
+                    del map[num+1]
+                    count+= num+1
+                    map[num+1] = num
+                else:
+                    map[num+1]-=1
+        return count
         
