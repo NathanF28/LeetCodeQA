ec70c832bb725a52615902035a638a9b5d3599ca
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-04-26 14:24:24 +0300

    Time: 7 ms (27.00%) | Memory: 18 MB (6.61%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        def inbound(row,col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-        rottens = []
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        rows = len(grid)
+        cols = len(grid[0]) 
+        q = deque()
         count = 0
-        visited = [ [False for _ in range(len(grid[0]))] for _ in range(len(grid)) ]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 2:
-                    rottens.append([i,j])
-                    visited[i][j] = True
-                if grid[i][j] == 1:
+                    q.append([i,j])
+                elif grid[i][j] == 1:
                     count+=1
-        res = -1
-        def bfs(oranges,time):
-            nonlocal visited
-            nonlocal count
-            nonlocal res
-            next = [] 
-            print(oranges,count,time)
+        minutes = 0
+        if count == 0:
+            return 0
+        def inbound(row,col):
+            return 0 <= row < rows and 0 <= col < cols
+        while q: 
+            print(q)
+            minutes+=1
+            for _ in range(len(q)):
+                row,col = q.popleft()
+                for dr,dc in directions:
+                    nr = row+dr
+                    nc = col+dc
+                    if inbound(nr,nc) and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        count-=1
+                        q.append([nr,nc])
             if count == 0:
-                res = time
-                return
-            for liste in oranges:
-                for direction in directions:
-                    new_row = liste[0] + direction[0]
-                    new_col = liste[1] + direction[1]
-                    if inbound(new_row,new_col) and not visited[new_row][new_col] and grid[new_row][new_col]:
-                        visited[new_row][new_col] = True
-                        if grid[new_row][new_col] == 1:
-                            count-=1
-                        next.append([new_row,new_col])
-            if len(next) != 0:
-                bfs(next,time+1)
-        bfs(rottens,0)
-        return res
+                break
+        return minutes if count == 0 else -1
 
