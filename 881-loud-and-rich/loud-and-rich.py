b8f6001a0f82b8bec10eeb5365319a525a12f8d9
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-04-26 09:12:33 +0300

    Time: 8 ms (94.43%) | Memory: 24.7 MB (24.70%) - LeetSync

---
+++ 881-loud-and-rich/loud-and-rich.py
@@ -0,0 +1,28 @@
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        graph = defaultdict(list)
+        n = len(quiet)
+        for u,v in richer:
+            graph[v].append(u)
+        
+        answer = [-1] * n
+        def dfs(i,_min):
+            nonlocal answer
+            if answer[i] != -1:
+                return answer[i]
+            if quiet[i] < quiet[_min]:
+                _min = i
+            val = _min
+            for neighbor in graph[i]:
+                candidate = dfs(neighbor, _min)
+                if quiet[candidate] < quiet[val]:
+                    val = candidate
+            return val
+        res = []
+        for i in range(n):
+            x = dfs(i,i)
+            answer[i] = x
+            res.append(x)
+        return res
+
+        
