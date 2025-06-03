72e2e6347520f269dbe6a4299906bf5fcf2aadc7
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-06-03 18:54:53 +0300

    Time: 95 ms (19.33%) | Memory: 32 MB (66.72%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,25 +1,8 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> List[int]:
-        
-        def merge(nums,l,r):
-            if l == r:
-                return nums[l]
-            mid = (l+r)//2 
-            left = merge(nums,l,mid)
-            right = merge(nums,mid+1,r)
-            cross = best_mid(nums,l,mid,r)
-            return max(left,right,cross)
-
-        def best_mid(nums,start,mid,end):
-            left_best = float('-inf')
-            tot = 0
-            for i in range(mid,start-1,-1):
-                tot+=nums[i]
-                left_best = max(left_best,tot)
-            right_best = float('-inf')
-            tot = 0
-            for i in range(mid+1,end+1):
-                tot+=nums[i]
-                right_best = max(right_best,tot)
-            return left_best + right_best
-        return merge(nums,0,len(nums)-1)
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_ending_here = nums[0]
+        max_so_far = nums[0]
+        for i in range(1,len(nums)):
+            max_ending_here = max(nums[i], max_ending_here + nums[i])
+            max_so_far = max(max_so_far,max_ending_here)
+        return max_so_far
