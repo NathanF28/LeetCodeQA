e666a3eb214734fcf63427ca20bcfe876ea50142
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-16 22:59:42 +0300

    Time: 39 ms (14.35%) | Memory: 19.7 MB (6.11%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,24 +1,39 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        # storing positions of ones first
+        positions = []
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]:
+                    positions.append((i,j))
+        max_squares = 0
+        visited = set()
+
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         def inbound(row,col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-        longest = 0
+            return 0 <= row < rows and 0 <= col < cols
+
         def dfs(row,col):
-            nonlocal visited
-            nonlocal longest
-            if not inbound(row,col) or visited[row][col] or grid[row][col] == 0:
-                return 0
-            visited[row][col] = True
-            val = 1
-            for direction in directions:
-                new_row = row + direction[0]
-                new_col= col + direction[1]
-                val += dfs(new_row,new_col)
-            longest = max(val,longest)
-            return val
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                dfs(i,j)
-        return longest
+            square = 1
+            for dr,dc in directions:
+                nr = dr + row
+                nc = dc + col
+                if inbound(nr,nc) and (nr,nc) not in visited and grid[nr][nc] != 0:
+                    visited.add((nr,nc))
+                    square += dfs(nr,nc)
+            return square
+
+
+        for x,y in positions:
+            if (x,y) not in visited:
+                visited.add((x,y))
+                val = dfs(x,y)
+                max_squares = max(max_squares, val)
+
+        return max_squares
+
+
+        
+        
