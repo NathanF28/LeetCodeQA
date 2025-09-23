c87673d2ded99ba2fc3190d10b95cb4d0f2f47ad
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-23 23:58:30 +0300

    Time: 135 ms (38.53%) | Memory: 74.2 MB (49.02%) - LeetSync

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -0,0 +1,45 @@
+class RandomizedCollection:
+
+    def __init__(self):
+        self.list = []
+        self.map = {}
+
+    def insert(self, val: int) -> bool:
+        self.list.append(val)
+        if val in self.map:
+            self.map[val].add(len(self.list)-1)
+            return False
+        self.map[val] = set()
+        self.map[val].add(len(self.list)-1)
+        return True
+
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        index = self.map[val].pop()
+        last_ind = len(self.list) - 1 
+        last_num = self.list[-1]
+        self.list[index] = last_num
+        
+        self.map[last_num].discard(last_ind)
+        if index != last_ind:
+            self.map[last_num].add(index)
+        self.list.pop()
+        if len(self.map[val]) == 0:
+            del self.map[val]
+
+        return True
+        
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+        
+
+
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
