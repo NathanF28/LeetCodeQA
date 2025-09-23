d09cf508cc4569cf34eb01e68ddcca9926b179d0
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-24 00:08:29 +0300

    Time: 127 ms (51.47%) | Memory: 74.2 MB (49.02%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -3,39 +3,42 @@ class RandomizedCollection:
     def __init__(self):
         self.list = []
         self.map = {}
+        
 
     def insert(self, val: int) -> bool:
-        self.list.append(val)
-        if val in self.map:
-            self.map[val].add(len(self.list)-1)
-            return False
-        self.map[val] = set()
-        self.map[val].add(len(self.list)-1)
-        return True
 
+        self.list.append(val)
+
+        if val in self.map:
+            self.map[val].add(len(self.list)-1) 
+            return False
+
+        self.map[val] = {len(self.list)-1}
+
+        return True
+        
+        
 
     def remove(self, val: int) -> bool:
         if val not in self.map:
             return False
-        index = self.map[val].pop()
-        last_ind = len(self.list) - 1 
-        last_num = self.list[-1]
-        self.list[index] = last_num
         
-        self.map[last_num].discard(last_ind)
-        if index != last_ind:
+        index = self.map[val].pop()
+
+        last_num = self.list[-1]
+
+        self.list[index] = last_num
+        self.map[last_num].discard(len(self.list)-1)
+        if len(self.list)-1 != index:
             self.map[last_num].add(index)
+
         self.list.pop()
         if len(self.map[val]) == 0:
-            del self.map[val]
-
-        return True
-        
-        
+            del self.map[val] 
+        return True 
 
     def getRandom(self) -> int:
-        return random.choice(self.list)
-        
+        return random.choice(self.list) 
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
