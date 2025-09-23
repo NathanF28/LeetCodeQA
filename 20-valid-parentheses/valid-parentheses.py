268088dc5fc6a2bfb394262be66e8db2e516668a
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-24 00:53:03 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (54.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,13 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        map = {')': '(', '}': '{', ']': '['}
+        map = {")": "(", "}" : "{" , "]": "["}
         stack = []
-        for bracket in s:
-            if bracket not in map:
-                stack.append(bracket)
-            else:
-                if stack and stack[-1] == map[bracket]:
+        for char in s:
+            if char in map:
+                if stack and stack[-1] != map[char]:
+                    return False
+                elif stack:
                     stack.pop()
                 else:
                     return False
+            else:
+                stack.append(char)
         return not stack
+                    
+        
