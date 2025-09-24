8efa6e2d629d0e4f2b685606f41925392b87352a
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-24 16:37:41 +0300

    Time: 52 ms (41.04%) | Memory: 34.5 MB (74.13%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,17 +1,18 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
-        possible = []
 
-        def split(path, string):
+
+        possible = [] 
+        def split(string,path):
             if len(string) == 0:
                 possible.append(path[:])
-                return 
+
             for i in range(len(string)):
-                substring = string[:i+1]
-                if substring == substring[::-1]:
-                    path.append(substring)
-                    split(path,string[i+1:])
+                sub = string[:i+1]
+                if sub == sub[::-1]:
+                    path.append(sub)
+                    split(string[i+1:],path)
                     path.pop()
-        split([], s)
+        split(s,[])
         return possible
+                
