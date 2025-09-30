5145cc63d7d619c3cc869ff8da43eb847cdc86c7
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-09-30 17:50:01 +0300

    Time: 5 ms (90.15%) | Memory: 19 MB (16.72%) - LeetSync

--- 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.py
+++ 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.py
@@ -1,20 +1,27 @@
+from bisect import bisect_left
+from typing import List
+
 class Solution:
     def findBestValue(self, arr: List[int], target: int) -> int:
         arr.sort()
         val = float('inf')
         od = float('inf')
-        
-        for i in range(arr[-1] + 1):
+        l = 0
+        r = arr[-1]
+
+        while l <= r:
+            i = (l + r) // 2
             pos = bisect_left(arr, i)
             sume = sum(arr[:pos]) + (len(arr) - pos) * i
             diff = abs(target - sume)
             
-            if diff < od:
+            if diff < od or (diff == od and i < val):
                 od = diff
                 val = i
-            elif diff == od:
-                val = min(val, i)
+
+            if sume < target:
+                l = i + 1
             else:
-                break
-        
+                r = i - 1
+
         return val
