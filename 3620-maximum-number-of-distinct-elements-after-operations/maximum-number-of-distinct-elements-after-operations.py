bfb8eb77d5029232322a251934eba5a62e58753e
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-10-18 08:31:49 +0300

    Time: 726 ms (62.13%) | Memory: 31.5 MB (76.92%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.py
@@ -1,20 +1,13 @@
-from typing import List
-
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
         nums.sort()
-        last_picked = -10**18
-        distinct_count = 0
+        prev = -float('inf')
+        d = 0
         for num in nums:
-            lower_bound = num - k
-            upper_bound = num + k
-            if last_picked < lower_bound:
-                last_picked = lower_bound
-            else:
-                last_picked += 1
-            if last_picked <= upper_bound:
-                distinct_count += 1
-            else:
-                last_picked -= 1
-
-        return distinct_count
+            target = prev + 1
+            _min = num - k
+            curr = max(_min,target)
+            if curr <= num + k:
+                prev = curr
+                d+=1
+        return d
