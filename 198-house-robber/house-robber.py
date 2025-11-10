b71c98f69f40cc596083672edc8d8c84f25f7ba9
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-11-10 10:59:49 +0300

    Time: 2 ms (9.06%) | Memory: 17.7 MB (53.72%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-
-
         dp = [0] * len(nums)
         dp[0] = nums[0]
 
         for i in range(1,len(nums)):
-            cand1 = dp[i-1]
-            cand2 = nums[i] + dp[i-2] if i > 1 else nums[i]
+            val = dp[i-2] if i - 2 >=0 else 0
+            cand1 = val + nums[i]
+            cand2 = dp[i-1]
             dp[i] = max(cand1,cand2)
         return dp[-1]
