b339a4103f0780b3e5e019de8288f714f1dfd749
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-11-12 09:55:29 +0300

    Time: 621 ms (7.07%) | Memory: 18.2 MB (76.87%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,16 +2,17 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tmap = Counter(t)
         smap = Counter()
-        shortest = ""
-        f = float('inf')
+        shortest = float('inf')
+        string = ""
         l = 0
         for r in range(len(s)):
             smap[s[r]]+=1
             while l < len(s) and smap[s[l]] > tmap[s[l]]:
-                smap[s[l]] -=1
+                smap[s[l]]-=1
                 l+=1
             if tmap <= smap:
-                if len(s[l:r+1]) < f:
-                    shortest = s[l:r+1]
-                    f = len(shortest)
-        return shortest
+                if r-l+1 < shortest:
+                    shortest = r-l+1
+                    string = s[l:r+1]
+        return string
+
