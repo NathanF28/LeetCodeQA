d9417baf200e214ea8cea96af2700397ee96fb1f
Nathan Fisseha <nathan12fisseha@gmail.com>
2025-12-01 11:32:07 +0300

    Time: 4 ms (45.49%) | Memory: 18.8 MB (62.47%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -7,18 +7,19 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
-        self.b = True
+        flag = True
+        if not root:
+            return flag
         def height(node):
+            nonlocal flag
             if not node:
                 return 0
-            
             left = height(node.left)
             right = height(node.right)
-
-            if abs(left - right) > 1:
-                self.b = False
-            
-            return 1 + max(left,right)
-        
+            curr = max(left,right)
+            if abs(left-right) > 1:
+                flag = False
+            return curr + 1
         height(root)
-        return self.b
+        return flag
+
