161acc01687dcbc298681635f1c96c6f31cee054
Nathan Fisseha <nathan12fisseha@gmail.com>
2026-02-26 19:11:42 +0300

    Time: 187 ms (25.93%) | Memory: 20.1 MB (15.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,25 +1,27 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        k = len(p)
-        if k > len(s):
-            return []
-        res = []
-        window = Counter()
-        map = Counter(p)
-        for i in range(k):
-            window[s[i]]+=1
-        
-        if window ==  map:
-            res.append(0)
-        left = 0
-        for i in range(k, len(s)):
-            window[s[i]]+=1
-            window[s[left]]-=1
-            if window[s[left]] == 0:
-                del window[s[left]] 
 
-            if window == map:
+        standard = Counter(p)
+        window_size = len(p)
+        rolling = Counter()
+        if len(p) > len(s):
+            return []
+        for i in range(window_size):
+            rolling[s[i]]+=1
+        count = 0 
+        res = []
+        if rolling == standard:
+            res.append(0)
+        left = 0 
+        for right in range(window_size,len(s)):
+            rolling[s[right]]+=1
+            rolling[s[left]]-=1
+            if rolling[s[left]] == 0:
+                del rolling[s[left]]
+            if rolling == standard:
                 res.append(left+1)
             left+=1
         return res
+        
+
 
